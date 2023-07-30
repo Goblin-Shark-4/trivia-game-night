98cc21edfcbff8f347b66235f3560d007cf4efc4
DE7741 <caviyun17@gmail.com>
2023-07-29 21:26:46 -0500

    Updated Front End

---
+++ client/components/QuestionCards.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import './QuestionCard.css';
+const QuestionCard = ({ question }) => {
+  const [showQuestion, setShowQuestion] = useState(true);
+
+  return (
+    <div
+      className={`question-card ${!showQuestion && 'flipped'}`}
+      onClick={() => setShowQuestion(!showQuestion)}
+    >
+      <div className='front'>
+        <h3>{question.category}</h3>
+        <h2>{question.difficulty}</h2>
+      </div>
+      <div className='back'>
+        <h3>{question.question}</h3>
+        {showQuestion ? null : (
+          <div>
+            <p>Correct Answer: {question.correct_answer}</p>
+            <p>Incorrect Answers: {question.incorrect_answers.join(', ')}</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionCard;
