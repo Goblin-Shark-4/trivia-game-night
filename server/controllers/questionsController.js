c3efacaf391ccb99cea1d9b37b83cb7ca69446b4
Denton Wong <denton.wong@gmail.com>
2023-08-02 12:49:30 -0700

    prettier

--- server/controllers/questionsController.js
+++ server/controllers/questionsController.js
@@ -7,18 +7,19 @@ questionsController.getQuestions = (req, res, next) => {
   const questions = {};
   // deliver questions to front-end
   Promise.all(
+    // eslint-disable-next-line consistent-return
     categories.map(async (category) => {
       try {
         const easy = await Topic.aggregate([
-          { $match: { category: category, difficulty: 'easy' } },
+          { $match: { category, difficulty: 'easy' } },
           { $sample: { size: 2 } },
         ]);
         const medium = await Topic.aggregate([
-          { $match: { category: category, difficulty: 'medium' } },
+          { $match: { category, difficulty: 'medium' } },
           { $sample: { size: 2 } },
         ]);
         const high = await Topic.aggregate([
-          { $match: { category: category, difficulty: 'hard' } },
+          { $match: { category, difficulty: 'hard' } },
           { $sample: { size: 1 } },
         ]);
         questions[category] = [...easy, ...medium, ...high];
@@ -27,17 +28,15 @@ questionsController.getQuestions = (req, res, next) => {
           log: `Express error handler caught an error at questionsController.getQuestions: ${err}`,
         });
       }
-    })
+    }),
   )
     .then(() => {
       res.locals.questions = questions;
       return next();
     })
-    .catch((err) =>
-      next({
-        log: `Express error handler caught an error at questionsController.getQuestions: ${err}`,
-      })
-    );
+    .catch((err) => next({
+      log: `Express error handler caught an error at questionsController.getQuestions: ${err}`,
+    }));
 };
 
 module.exports = questionsController;
